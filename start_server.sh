d18eefc1653169bc2ca5de35b4527c41d6f11008
kippiger <121905617+kippiger@users.noreply.github.com>
2023-02-11 23:34:00 +0100

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-~/ark/ShooterGame/Binaries/Linux/ShooterGameServer $ARKMAP?listen?SessionName=$ARKSESSIONNAME?ServerPassword=$ARKSERVERPW?ServerAdminPassword=$ARKADMINPW?RconEnabled=true -server -log -NoBattlEye
+~/ark/ShooterGame/Binaries/Linux/ShooterGameServer $ARKMAP?listen?SessionName=$ARKSESSIONNAME?ServerPassword=$ARKSERVERPW?ServerAdminPassword=$ARKADMINPW?RconEnabled=true -server -log $BATTLEYE
